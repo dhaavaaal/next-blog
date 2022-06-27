acc6ffc207181e86a4d429c3eb09d1396067d57b
Dhaval Thakkar <dhavalthakkar1541@gmail.com>
2022-06-27 15:11:42 +0530

    Changes in time component

--- components/TimeSection.tsx
+++ components/TimeSection.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 const TimeSection = ({ dateTime }) => {
-  console.log(dateTime);
-  return <div>TimeSection: {dateTime}</div>;
+  let splittingTime, time;
+  if (dateTime) {
+    splittingTime = dateTime.split("T");
+    time = splittingTime[1].substring(0, 8);
+  }
+  return <div>{time && <div>Time: {time}</div>}</div>;
 };
 
 export default TimeSection;
